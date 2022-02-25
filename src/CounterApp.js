e7d8445f9a8696763f8f6b23d2336b916900bc69
Uriel <uriellopezaquino1.0@gmail.com>
2022-02-24 19:02:31 -0600

    UseState & HandleSubstract y Handlerset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
 
-    //HandleAdd
-    const handleAdd = (e) =>{
-        console.log(e)
+    const [ counter, setCounter ] = useState( value ); // []
+    
+
+    // handleAdd
+    const handleAdd = () => {
+        setCounter( counter + 1);
+        // setCounter( (c) => c + 1 );
     }
 
-  return (
-    <div>
-        <h1>CounterApp</h1>
-        <h2>{ value }</h2>  
+    const handleSubtract = () => setCounter( counter - 1);
+    
+    const handleReset = () => setCounter( value );
 
-        <button onClick={handleAdd}>+1</button>
 
-    </div>
-  )
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { counter } </h2>
+
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubtract }>-1</button>
+        </>
+    )
 }
 
 CounterApp.propTypes = {
-    value : PropTypes.number
+    value: PropTypes.number
 }
 
-export default CounterApp
+export default CounterApp;
